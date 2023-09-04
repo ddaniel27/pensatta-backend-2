a1a05b4a9cb478c99e1ca42750f7ad325da6865d
ddaniel27 <danieldorado27@gmail.com>
2023-09-04 16:28:10 -0500

    [UPDATE] login

--- components/login/handler.js
+++ components/login/handler.js
@@ -35,13 +35,11 @@ module.exports = () => {
             .json({ message: 'Invalid username or password' })
         }
 
-        /*
         if (!validate(password, cred.password)){
           return res
             .status(401)
             .json({ message: 'Invalid username or password' })
         }
-        */
 
         req
           .session
