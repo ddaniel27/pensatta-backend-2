e0591a3fb3ad0cc9bc1c2a5fa588424c6f40cd58
ddaniel27 <danieldorado27@gmail.com>
2023-09-08 17:20:19 -0500

    [UPDATE] Institution updated

--- components/institution/handler.js
+++ components/institution/handler.js
@@ -8,29 +8,29 @@ module.exports = () => {
     .get(async (_req, res) => {
       try {
         const institutions = await db.getInstitutions()
-        res.status(200).json(institutions)
+        res.status(200).json({ institutions })
       } catch (e) {
         res.status(400).send(e.message)
       }
     })
     .post(async (req, res) => {
       const {
-        nombre: name,
+        name,
         email,
-        pais,
-        provincia,
-        ciudad,
-        codigo
+        country,
+        province,
+        city,
+        institution_code
       } = req.body
 
       try {
         const result = await db.insertNewInstitution({
           name,
           email,
-          pais,
-          provincia,
-          ciudad,
-          codigo
+          pais: country,
+          provincia: province,
+          ciudad: city,
+          codigo: institution_code
         })
         res.status(200).json(result)
       }
